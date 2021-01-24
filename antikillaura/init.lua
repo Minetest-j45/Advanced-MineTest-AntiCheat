a12679d81110c8d4df60ec5d224dd3caf3d45c87
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-01-24 21:15:55 +0000

    Update init.lua

--- antikillaura/init.lua
+++ antikillaura/init.lua
@@ -1,1 +1,3 @@
 on punchplayer if not hitterget poointedthing pos = player get pos then kick hitter
+minetest.register_on_punchplayer(function(player, hitter, time_from_last_punch, tool_capabilities, dir, damage))
+minetest.kick_player(name, Using killaura hacks.)
