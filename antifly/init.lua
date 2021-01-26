11f5b2054767adb0adad30bd2576c4b5918a6924
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-01-26 14:15:09 +0100

    Aaaaaand try 3

--- antifly/init.lua
+++ antifly/init.lua
@@ -7,7 +7,8 @@ minetest.register_globalstep(function(dtime)
                 if minetest.get_node({pos.x, pos.y - 1, pos.z}).name ~= "air" then
                     if minetest.get_node({pos.x, pos.y - 2, pos.z}).name ~= "air" then
                         if minetest.get_node({pos.x, pos.y - 3, pos.z}).name ~= "air" then
-                            if player:get_velocity().y > 0.1 then
+                            local playerVelocity = player:get_player_velocity()
+                            if playerVelocity.y >= 0.1 then
                                 minetest.kick_player(playerName, "AMTAC: Fly")
                             end
                         end
