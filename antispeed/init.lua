3e07c26314826a7f475bff72f52aeda0f3e85118
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-01-26 15:51:23 +0100

    add beta-antispeed

---
+++ antispeed/init.lua
@@ -0,0 +1,21 @@
+minetest.register_globalstep(function(dtime)
+    local players = minetest.get_connected_players();
+        for _,player in pairs(players) do
+            if(player) then
+                local playerName = player:get_player_name();
+                local playerVelocity = player:get_velocity()
+                if playerVelocity.x > 5 then
+                    minetest.kick_player(playerName, "AMTAC: Speed")
+                end
+                if playerVelocity.x < -5 then
+                    minetest.kick_player(playerName, "AMTAC: Speed")
+                end
+                if playerVelocity.z > 5 then
+                    minetest.kick_player(playerName, "AMTAC: Speed")
+                end
+                if playerVelocity.z < -5 then
+                    minetest.kick_player(playerName, "AMTAC: Speed")
+                end
+            end
+		end
+end)
