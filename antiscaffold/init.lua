e18298c0f9288022e60f505e05b956804d7c4497
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-01-26 14:23:14 +0000

    fix some bugs(still not working tho)

--- antiscaffold/init.lua
+++ antiscaffold/init.lua
@@ -1,6 +1,8 @@
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
     if not placer then return end
-    if not pos == pointed_thing.under
+    print("pos: " .. " " .. pos.x .. " " .. pos.y .. " " .. pos.z)
+    print("pt: " .. pointed_thing.above.x .. " " .. pointed_thing.above.y .. " " .. pointed_thing.above.z)
+    if not pos == pointed_thing.above then
         minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold")
     end
 end)
