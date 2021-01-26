3d97e9d0c176c437b28e9f53e373fcbaace0eea0
HimbeerserverDE <lpaschek@icloud.com>
2021-01-26 16:56:14 +0100

    Rewrite antifly

--- antifly/init.lua
+++ antifly/init.lua
@@ -1,19 +1,42 @@
-minetest.register_globalstep(function(dtime)
-    local players = minetest.get_connected_players();
-        for _,player in pairs(players) do
-            if(player) then
-                local playerName = player:get_player_name();
-                local pos = player:get_pos();
-                if minetest.get_node({pos.x, pos.y - 1, pos.z}).name ~= "air" then
-                    if minetest.get_node({pos.x, pos.y - 2, pos.z}).name ~= "air" then
-                        if minetest.get_node({pos.x, pos.y - 3, pos.z}).name ~= "air" then
-                            local playerVelocity = player:get_player_velocity()
-                            if playerVelocity.y >= 0.01 then
-                                minetest.kick_player(playerName, "AMTAC: Fly")
-                            end
-                        end
-                    end
-                end
-            end
+local oldpos = {}
+
+local function fly_check(player)
+	local name = player:get_player_name()
+	local pos = player:get_pos()
+	if minetest.check_player_privs(name, {fly = true}) then
+		return true
+	end
+	if minetest.get_node(pos).name == "air" and minetest.get_node({x = pos.x, y = pos.y - 1, z = pos.z}).name == "air" then
+		local airnodes = minetest.find_nodes_in_area({x = pos.x - 2, y = pos.y - 2, z = pos.z - 2}, {x = pos.x + 2, y = pos.y + 2, z = pos.z + 2}, {"air"})
+		local v = player:get_velocity()
+		if #airnodes >= 125 and v.y >= 0 then
+			return false
 		end
+	end
+
+	return true
+end
+
+local function posdel(player)
+	if fly_check(player) then
+		oldpos[player:get_player_name()] = nil
+	else
+		minetest.after(3, posdel, player)
+	end
+end
+
+minetest.register_globalstep(function(dtime)
+	local players = minetest.get_connected_players()
+	for _, player in ipairs(players) do
+		local name = player:get_player_name()
+		local pos = player:get_pos()
+		if not fly_check(player) then
+			if oldpos[name] then
+				player:set_pos(oldpos[name])
+			end
+			minetest.log("action", "[AMTAC] Fly detected for " .. name)
+			if not oldpos[name] then oldpos[name] = pos end
+			minetest.after(3, posdel, player)
+		end
+	end
 end)
