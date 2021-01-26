b42f50daaa83c720b3fa899920f5ef23104a102c
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-01-26 14:45:59 +0100

    Fix antinoclip-ban

--- antinoclip/init.lua
+++ antinoclip/init.lua
@@ -15,7 +15,9 @@ minetest.register_globalstep(function(dtime)
 			end
 			if not clear then -- player inside wall
 				player:set_hp(0)
-				minetest.kick_player(playerName, "AMTAC: NoClip")
+				minetest.after(0.05, function()
+					minetest.kick_player(playerName, "AMTAC: NoClip")
+				end)
 			end
 		end
 end)
