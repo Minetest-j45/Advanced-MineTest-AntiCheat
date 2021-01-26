2ce8f181be90b0b99347ee45a96e1644dcffd429
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-01-26 16:02:19 +0100

    fix antispeed try 2

--- antispeed/init.lua
+++ antispeed/init.lua
@@ -1,19 +1,20 @@
 minetest.register_globalstep(function(dtime)
+    local SpeedLimit = 1
     local players = minetest.get_connected_players();
         for _,player in pairs(players) do
             if(player) then
                 local playerName = player:get_player_name();
                 local playerVelocity = player:get_player_velocity()
-                if playerVelocity.x > 5 then
+                if playerVelocity.x > SpeedLimit then
                     minetest.kick_player(playerName, "AMTAC: Speed")
                 end
-                if playerVelocity.x < -5 then
+                if playerVelocity.x < -SpeedLimit then
                     minetest.kick_player(playerName, "AMTAC: Speed")
                 end
-                if playerVelocity.z > 5 then
+                if playerVelocity.z > SpeedLimit then
                     minetest.kick_player(playerName, "AMTAC: Speed")
                 end
-                if playerVelocity.z < -5 then
+                if playerVelocity.z < -SpeedLimit then
                     minetest.kick_player(playerName, "AMTAC: Speed")
                 end
             end
