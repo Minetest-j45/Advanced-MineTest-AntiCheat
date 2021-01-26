72a23ffc5f431fed0fa3e9470f8dc9044b1bea0a
Zander200-glitch <60350322+Zander200-glitch@users.noreply.github.com>
2021-01-26 14:44:33 +0100

    fly test 5

--- antifly/init.lua
+++ antifly/init.lua
@@ -3,7 +3,7 @@ minetest.register_globalstep(function(dtime)
         for _,player in pairs(players) do
             if(player) then
                 local playerName = player:get_player_name();
-                local pos = player:getpos();
+                local pos = player:get_pos();
                 if minetest.get_node({pos.x, pos.y - 1, pos.z}).name ~= "air" then
                     if minetest.get_node({pos.x, pos.y - 2, pos.z}).name ~= "air" then
                         if minetest.get_node({pos.x, pos.y - 3, pos.z}).name ~= "air" then
