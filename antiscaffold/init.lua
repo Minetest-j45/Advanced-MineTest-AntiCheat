9b2d7a330c4f8129d93786f18919a66ff14adc7f
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-01-26 13:26:39 +0000

    Change kick message for antiscaffold

--- antiscaffold/init.lua
+++ antiscaffold/init.lua
@@ -1,6 +1,6 @@
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
     if not placer then return end
     if not pos == pointed_thing.under
-        minetest.kick_player(placer:get_player_name(), "Kicked " .. placer:get_player_name() .. " for using scaffold hacks.")
+        minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold")
     end
 end)
