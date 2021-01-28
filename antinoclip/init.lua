0b6824241b21a64f80763ff118aa34a45c674feb
HimbeerserverDE <lpaschek@icloud.com>
2021-01-28 17:49:54 +0100

    Rewrite antinoclip

--- antinoclip/init.lua
+++ antinoclip/init.lua
@@ -1,24 +1,33 @@
-nodes = {["default:stone"] = false, ["default:cobble"]= false, ["default:dirt"] = false, ["default:sand"]=false,["default:tree"]= false};
-moderators = {["Zander_200"]=true};
+local oldpos = {}
+
+local function noclip_check(player)
+	local name = player:get_player_name()
+	local pos = player:get_pos()
+	-- if minetest.check_player_privs(name, {noclip = true}) then
+		-- return true
+	-- end
+	if minetest.get_node(pos).name ~= "air" and minetest.get_node({x = pos.x, y = pos.y - 1, z = pos.z}).name ~= "air" then
+		local v = player:get_velocity()
+		if v.x ~= 0 or v.y ~= 0 or v.z ~= 0 then
+			return false
+		end
+	end
+
+	return true
+end
 
 minetest.register_globalstep(function(dtime)
-	local players = minetest.get_connected_players();
-		for _,player in pairs(players) do
-			local playerName = player:get_player_name();
-			local pos = player:getpos();
-			if pos.y<0 then pos.y=pos.y+1 end
-			local nodename = minetest.get_node(pos).name;
-			local clear=true;
-			if nodename ~= "air" then  -- check material
-				clear = nodes[nodename]; -- test clip
-				if clear == nil then clear = true end
+	local players = minetest.get_connected_players()
+	for _, player in ipairs(players) do
+		local name = player:get_player_name()
+		local pos = player:get_pos()
+		if not noclip_check(player) then
+			if oldpos[name] then
+				player:set_pos(oldpos[name])
 			end
-			if not clear then -- player inside wall
-				player:set_pos(vector.add(pos, {x = 0, y = 100, z = 0}))
-				player:set_hp(0)
-				minetest.after(0.05, function()
-					minetest.kick_player(playerName, "AMTAC: NoClip")
-				end)
-			end
+			minetest.log("action", "[AMTAC] Noclip detected for " .. name)
+		else
+			oldpos[name] = player:get_pos()
 		end
+	end
 end)
