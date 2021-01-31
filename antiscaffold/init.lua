9aff09102eb9c31606d3df2037cb7fc1db9b403a
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2021-01-31 13:59:31 +0000

    Fix broken in pre-5.4.0 servers

--- antiscaffold/init.lua
+++ antiscaffold/init.lua
@@ -1,9 +1,18 @@
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
         if placer:is_player() then
                 local control = placer:get_player_control()
+                local pre540 = false
 
-                if not control.place then
-                        minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement")
+                if control.place == nil then pre540 = true end -- Check for pre 5.4.0 servers
+
+                if pre540 ~= true then
+                        if not control.place then
+                                minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement!")
+                        end
+                else
+                        if not control.RMB then
+                                minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement!")
+                        end
                 end
         end
 end)
