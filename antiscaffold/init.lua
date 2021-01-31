90ed7fce890ec56500dc5d4dbbb3ca01a63a28d9
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-01-31 12:16:36 +0000

    Update init.lua

--- antiscaffold/init.lua
+++ antiscaffold/init.lua
@@ -3,7 +3,7 @@ minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack
                 local control = placer:get_player_control()
 
                 if not control.place then
-                        minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold!")
+                        minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement")
                 end
         end
 end)
