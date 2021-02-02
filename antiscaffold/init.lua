8879bb78b89b4b447d5b591ba26fda59a8a29e97
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2021-02-02 11:16:37 +0000

    Remove unnessacary check for pre 5.4.0 servers

--- antiscaffold/init.lua
+++ antiscaffold/init.lua
@@ -3,21 +3,10 @@ minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack
 	if placer:is_player() then
 		local playerName = placer:get_player_name()
 		local control = placer:get_player_control()
-		local pre540 = false
-
-		if control.place == nil then pre540 = true end -- Check for pre 5.4.0 servers
-
-		-- TODO: Revert action and increment cheat counter instead of kicking
-		if pre540 ~= true then
-			if not control.place then
-				minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement!")
-				minetest.log("[AMTAC]: " .. playerName .. " was caught using scaffold hacks")
-			end
-		else
-			if not control.RMB then
-				minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement!")
-				minetest.log("[AMTAC]: " .. playerName .. " was caught using scaffold hacks")
-			end
+			
+		if not control.RMB then
+			minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement!")
+			minetest.log("[AMTAC]: " .. playerName .. " was caught using scaffold hacks")
 		end
 	end
 end)
