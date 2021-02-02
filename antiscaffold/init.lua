51b99d0bc6c37823e5930354c61e6ce3b960358f
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2021-02-02 10:17:28 +0000

    Fix #1

--- antiscaffold/init.lua
+++ antiscaffold/init.lua
@@ -1,4 +1,5 @@
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
+        if placer == nil then return end
         if placer:is_player() then
                 local playerName = placer:get_player_name()
                 local control = placer:get_player_control()
@@ -8,13 +9,13 @@ minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack
 
                 if pre540 ~= true then
                         if not control.place then
-                                minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement")
-                                minetest.log("[AMTAC]: .. " playerName .. " was caught using scaffold or other abnormal block placement hacks")
+                                minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement!")
+                                minetest.log("[AMTAC]: " .. playerName .. " was caught using scaffold hacks")
                         end
                 else
                         if not control.RMB then
-                                minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement")
-                                minetest.log("[AMTAC]: .. " playerName .. " was caught using scaffold or other abnormal block placement hacks")
+                                minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement!")
+                                minetest.log("[AMTAC]: " .. playerName .. " was caught using scaffold hacks")
                         end
                 end
         end
