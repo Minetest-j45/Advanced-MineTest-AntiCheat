ae18a16a4257eebae1715f227c40c2dd32c802ed
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2021-02-02 11:19:59 +0000

    Change log type from text to action

--- antikillaura/init.lua
+++ antikillaura/init.lua
@@ -8,6 +8,6 @@ minetest.register_on_punchplayer(function(player, hitter, time_from_last_punch, 
 
         if not control.LMB then
                 minetest.kick_player(hitter:get_player_name(), "[AMTAC]: Killaura!")
-                minetest.log("[AMTAC]: " .. hitter:get_player_name() .. " was caught using killaura")
+                minetest.log("action", "[AMTAC]: " .. hitter:get_player_name() .. " was caught using killaura")
         end
 end)
