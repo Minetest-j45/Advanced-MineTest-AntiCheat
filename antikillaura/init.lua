37e22ba47989cca316b4ae495604cff2b428403d
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2021-02-02 11:12:11 +0000

    Add more error checking for antikillaura

--- antikillaura/init.lua
+++ antikillaura/init.lua
@@ -1,9 +1,13 @@
 minetest.register_on_punchplayer(function(player, hitter, time_from_last_punch, tool_capabilities, dir, damage)
-	local control = hitter:get_player_control()
+        if hitter == nil then return end
+        if player == nil then return end
+        if not hitter:is_player() then return end
+        if not player:is_player() then return end
 
-	if not control.LMB then
-		-- TODO: Revert action and increment cheat counter instead of kicking
-		minetest.kick_player(hitter:get_player_name(), "[AMTAC]: Killaura")
-		minetest.log("[AMTAC]: " .. hitter:get_player_name() .. " was caught using killaura")
-	end
+        local control = hitter:get_player_control()
+
+        if not control.LMB then
+                minetest.kick_player(hitter:get_player_name(), "[AMTAC]: Killaura!")
+                minetest.log("[AMTAC]: " .. hitter:get_player_name() .. " was caught using killaura")
+        end
 end)
