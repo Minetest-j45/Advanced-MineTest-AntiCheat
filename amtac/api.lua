11c96dc3016032d71ecd930db7693670be167d16
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2021-02-05 15:11:33 +0000

    Add punishment option to the option table in amtac.handle_cheater()

--- amtac/api.lua
+++ amtac/api.lua
@@ -13,24 +13,30 @@ function amtac.notify_all_of_cheater(player, reason)
         minetest.chat_send_all(player:get_player_name() .. " was caught using " .. reason:lower())
 end
 
-function amtac.handle_cheater(player, reason, punishment, options)
-        if not player or not reason or not punishment then return end
+function amtac.handle_cheater(player, reason, options)
+        if not player or not reason or not options then return end
 
-        if punishment == "kick" then
-                amtac.kick(player, "AMTAC: " .. reason)
+        amtac.kick(player, "AMTAC: " .. reason)
 
-                if options.log then
-                        minetest.log("action", "AMTAC " .. player:get_player_name() .. " was caught using " .. reason:lower())
+        if options.log then
+                minetest.log("action", "AMTAC " .. player:get_player_name() .. " was caught using " .. reason:lower())
+        end
+
+        if options.kill then
+                player:set_hp(0)
+        end
+
+        if options.notify_all then
+                amtac.notify_all_of_cheater(player, reason)
+        end
+
+        if options.punishment then
+                local punishment = options.punishment
+
+                if punishment == "kick" then
+                        amtac.kick(player, reason)
+                else
+                        amtac.ban(player, reason)
                 end
-
-                if options.kill then
-                        player:set_hp(0)
-                end
-
-                if options.notify_all then
-                        amtac.notify_all_of_cheater(player, reason)
-                end
-        else
-                amtac.ban(player, "AMTAC: " .. reason)
         end
 end
