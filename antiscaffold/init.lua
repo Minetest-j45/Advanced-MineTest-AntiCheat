f10446cc9f8e16a38760a2ffd86576c017679f8b
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2021-02-05 12:44:55 +0000

    Make AntiScaffold use the API

--- antiscaffold/init.lua
+++ antiscaffold/init.lua
@@ -1,12 +1,10 @@
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
 	if placer == nil then return end
 	if placer:is_player() then
-		local playerName = placer:get_player_name()
 		local control = placer:get_player_control()
 			
 		if not control.RMB then
-			minetest.kick_player(placer:get_player_name(), "AMTAC: Scaffold or other abnormal block placement!")
-			minetest.log("action", "[AMTAC]: " .. playerName .. " was caught using scaffold hacks")
+				amtac.handle_cheater(placer, "Scaffold", "kick", {log = true, kill = true, notify_all = true})
 		end
 	end
 end)
