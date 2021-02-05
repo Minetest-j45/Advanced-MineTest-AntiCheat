6fb33da3cec2e299b6722d24334d5c7662e4f119
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2021-02-05 12:47:41 +0000

    Improve AntiSpeed and make it use the API

--- antispeed/init.lua
+++ antispeed/init.lua
@@ -1,26 +1,13 @@
 minetest.register_globalstep(function(dtime)
-    local SpeedLimit = 1
-    local players = minetest.get_connected_players();
+    local SpeedLimit = minetest.settings:get("movement_speed_walk")
+    local players = minetest.get_connected_players()
         for _,player in pairs(players) do
             if(player) then
-                local playerName = player:get_player_name();
                 local playerVelocity = player:get_player_velocity()
-                if playerVelocity.x > SpeedLimit then
-                    minetest.kick_player(playerName, "AMTAC: Speed")
-		    minetest.log("action", "[AMTAC]: " .. playerName .. " was caught using speed hacks")
-                end
-                if playerVelocity.x < -SpeedLimit then
-                    minetest.kick_player(playerName, "AMTAC: Speed")
-		    minetest.log("action", "[AMTAC]: " .. playerName .. " was caught using speed hacks")
-                end
-                if playerVelocity.z > SpeedLimit then
-                    minetest.kick_player(playerName, "AMTAC: Speed")
-		    minetest.log("action", "[AMTAC]: " .. playerName .. " was caught using speed hacks")
-                end
-                if playerVelocity.z < -SpeedLimit then
-                    minetest.kick_player(playerName, "AMTAC: Speed")
-		    minetest.log("action", "[AMTAC]: " .. playerName .. " was caught using speed hacks")
-                end
+				
+                if playerVelocity.x > SpeedLimit or playerVelocity.x < -SpeedLimit or playerVelocity.z > SpeedLimit or playerVelocity.z < -SpeedLimit then
+			amtac.handle_cheater(player, "Speed", "kick", {log = true, kill = true, notify_all})
+		end
             end
 		end
 end)
