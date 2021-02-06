df2c6500d8f71a7d7845b1ae5276bd57f7ce251d
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2021-02-06 07:39:28 +0000

    Fix stupid typo

--- antispeed/init.lua
+++ antispeed/init.lua
@@ -6,7 +6,7 @@ minetest.register_globalstep(function(dtime)
                 local playerVelocity = player:get_player_velocity()
 				
                 if playerVelocity.x > SpeedLimit or playerVelocity.x < -SpeedLimit or playerVelocity.z > SpeedLimit or playerVelocity.z < -SpeedLimit then
-			amtac.handle_cheater(player, "Speed", {punishment = "kick", log = true, kill = true, notify_all})
+			amtac.handle_cheater(player, "Speed", {punishment = "kick", log = true, kill = true, notify_all = true})
 		end
             end
 		end
